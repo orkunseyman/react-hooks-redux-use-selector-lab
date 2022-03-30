0e2a8cd9f95e3cf909d12daeba34d84754ee9655
orkunseyman <orkunseyman@gmail.com>
2022-03-31 00:20:34 +0300

    f

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,19 +1,19 @@
 const initialState = {
-  users: [],
-};
-
-function usersReducer(state = initialState, action) {
-  switch (action.type) {
-    case "users/add":
-      console.log("adding ", action.payload);
-      return {
-        ...state,
-        users: [...state.users, action.payload],
-      };
-
-    default:
-      return state;
+    users: [],
+  };
+  
+  function usersReducer(state = initialState, action) {
+    switch (action.type) {
+      case "users/add":
+        console.log("adding ", action.payload);
+        return {
+          ...state,
+          users: [...state.users, action.payload],
+        };
+  
+      default:
+        return state;
+    }
   }
-}
-
-export default usersReducer;
+  
+  export default usersReducer;
